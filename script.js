3abf7412411bb3b276a6332627b50d1e0dbdaaf6
rizqy110 <aldinmodo@gmail.com>
2022-09-08 13:25:26 +0700

    update

--- script.js
+++ script.js
@@ -17,12 +17,18 @@ function getUrlParam(parameter, defaultvalue){
 
 var med = getUrlParam('med',null);
 var heg = getUrlParam('heg',null);
+var num = getUrlParam('num',null);
+var nim = getUrlParam('nim',null);
 var ti = getUrlParam('ti',null);
 	
 var encryptmed = decodeURIComponent(med);
 var encryptheg = decodeURIComponent(heg);
+var encryptnum = decodeURIComponent(num);
+var encryptnim = decodeURIComponent(nim);
 var encryptti = decodeURIComponent(ti);
 
 var tle = "<h3 class='ta-c'>"+ encryptti +" Sub Indo</h3>";
 
 var stream2 = "<source src='//archive.org/download/"+ encryptmed +".mp4' title='480p' type='video/mp4'/><source src='//archive.org/download/"+ encryptheg +".mp4' title='720p' type='video/mp4'/>";
+
+var xstream = "<source src='https://video"+ encryptnum +".tizam.cc/"+ encryptnim +".mp4' title='480p' type='video/mp4'/>";
